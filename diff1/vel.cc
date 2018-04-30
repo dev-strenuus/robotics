466d4fc458f10bb668f4b0a3bfa4705813cfbfe8
dev-strenuus <andrea5.scotti@mail.polimi.it>
2018-04-30 22:23:09 +0000

    script examples

---
+++ diff1/vel.cc
@@ -0,0 +1,50 @@
+#include <functional>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <ignition/math/Vector3.hh>
+#include <gazebo/physics/Joint.hh>
+#include <stdio.h>
+#include <time.h>
+#include <chrono>
+namespace gazebo
+{
+  class Vel : public ModelPlugin
+  {
+        std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+
+ int i=0;
+  private: physics::ModelPtr model;
+
+private: physics::JointPtr joint1_;
+
+ 	private: event::ConnectionPtr updateConnection;
+  
+  
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+    {
+      this->model = _parent;
+	this->joint1_ = this->model->GetJoint("left_wheel_hinge");	
+      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+          std::bind(&Vel::OnUpdate, this));
+    }
+public: void OnUpdate()
+    {
+
+
+std::chrono::time_point<std::chrono::system_clock> foo = std::chrono::system_clock::now();
+	   auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(foo - now);
+		if(milliseconds.count()<50)
+			return;
+	    now = std::chrono::system_clock::now();
+           double vel =  this->joint1_->GetVelocity(0);
+//this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
+            printf ("%d: %lf\n",i++,  vel);
+    }
+ 
+  };
+
+  GZ_REGISTER_MODEL_PLUGIN(Vel)
+}
+
+
