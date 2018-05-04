1ec51c7e61db112d5f6d71ad7afdf81c17e6faea
Gabriele <gabriele1.stucchi@mail.polimi.it>
2018-05-04 18:14:35 +0200

    finish yeahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- build/CMakeFiles/vel.dir/DependInfo.cmake
+++ build/CMakeFiles/vel.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/root/model_editor_models/progetto/vel.cc" "/root/model_editor_models/progetto/build/CMakeFiles/vel.dir/vel.cc.o"
+  "/home/gabriele/model_editor_models/progetto/vel.cc" "/home/gabriele/model_editor_models/progetto/build/CMakeFiles/vel.dir/vel.cc.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /root/model_editor_models/progetto
+# Install script for directory: /home/gabriele/model_editor_models/progetto
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/root/model_editor_models/progetto/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/gabriele/model_editor_models/progetto/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
